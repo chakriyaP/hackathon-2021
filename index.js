eed77ebdf5c751c00c8bdf0d075ea15f95d65fe4
chakriyaP <P.chakri>
2021-03-13 11:22:09 +0700

    add path webhok

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-//ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send('Hello World'))
+app.post('/webhook', (req, res) => res.sendStatus(200))
 
 app.listen(PORT, () => {
-
     console.log(`Server is running on port : ${PORT}`)
 })
 
