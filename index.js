62fc93f2b5e5ba3c74e8fe8393af70b31f3f5644
chakriyaP <P.chakri>
2021-03-13 14:36:17 +0700

    edit text

--- index.js
+++ index.js
@@ -28,178 +28,255 @@ function reply(reply_token) {
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-  "type": "flex",
-  "altText": "Flex Message",
-  "contents": {
-    "type": "carousel",
-    "contents": [
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "url": "https://firebasestorage.googleapis.com/v0/b/hackathon-5c71e.appspot.com/o/product1.jpg?alt=media&token=62926088-754b-44f9-bb91-89303c2380bd&fbclid=IwAR2UeRri4ThGSJ_UPeCv-9J-xnqQPtrsMUzG4E4Q0DqSur8demg-AYp3tPs"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Arm Chair, White",
-              "wrap": true,
-              "weight": "bold",
-              "size": "xl"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
+            "type": "flex",
+            "altText": "Flex Message",
+            "contents": {
+              "type": "carousel",
               "contents": [
                 {
-                  "type": "text",
-                  "text": "$49",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "xl"
+                  "type": "bubble",
+                  "hero": {
+                    "type": "image",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "url": "https://firebasestorage.googleapis.com/v0/b/hackathon-5c71e.appspot.com/o/product1.jpg?alt=media&token=62926088-754b-44f9-bb91-89303c2380bd"
+                  },
+                  "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "ก๋วยเตี๋ยวเอ็นตุ๋นเนื้อตุ๋นสมุนไพร",
+                        "wrap": true,
+                        "weight": "bold",
+                        "size": "xl"
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "69 บาท",
+                            "wrap": true,
+                            "weight": "bold",
+                            "size": "xl"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "button",
+                        "style": "primary",
+                        "action": {
+                          "type": "uri",
+                          "label": "สั่งซื้อสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "action": {
+                          "type": "uri",
+                          "label": "ดูรายละเอียดสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      }
+                    ]
+                  }
                 },
                 {
-                  "type": "text",
-                  "text": ".99",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "sm"
+                  "type": "bubble",
+                  "hero": {
+                    "type": "image",
+                    "url": "https://firebasestorage.googleapis.com/v0/b/hackathon-5c71e.appspot.com/o/product2.jpg?alt=media&token=c463e892-a887-485a-9bd3-20f81876d772",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover"
+                  },
+                  "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "ก๋วยเตี๋ยวหมูตุ๋นสมุนไพร",
+                        "size": "xl",
+                        "weight": "bold"
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "69 บาท",
+                            "weight": "bold",
+                            "size": "xl"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                      {
+                        "type": "button",
+                        "action": {
+                          "type": "uri",
+                          "label": "สั่งซื้อสินค้า",
+                          "uri": "http://linecorp.com/"
+                        },
+                        "style": "primary"
+                      },
+                      {
+                        "type": "button",
+                        "action": {
+                          "type": "uri",
+                          "label": "ดูรายละเอียดสินค้า",
+                          "uri": "http://linecorp.com/"
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  "type": "bubble",
+                  "hero": {
+                    "type": "image",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "url": "https://firebasestorage.googleapis.com/v0/b/hackathon-5c71e.appspot.com/o/product3.jpg?alt=media&token=426e674c-f622-4251-86ae-11f6d4992f30"
+                  },
+                  "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "ก๋วยเตี๋ยวเป็ดตุ๋นสมุนไพร",
+                        "wrap": true,
+                        "weight": "bold",
+                        "size": "xl"
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "69 บาท",
+                            "wrap": true,
+                            "weight": "bold",
+                            "size": "xl"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "button",
+                        "style": "primary",
+                        "action": {
+                          "type": "uri",
+                          "label": "สั่งซื้อสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "action": {
+                          "type": "uri",
+                          "label": "ดูรายละเอียดสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      }
+                    ]
+                  }
+                },
+                {
+                  "type": "bubble",
+                  "hero": {
+                    "type": "image",
+                    "size": "full",
+                    "aspectRatio": "20:13",
+                    "aspectMode": "cover",
+                    "url": "https://firebasestorage.googleapis.com/v0/b/hackathon-5c71e.appspot.com/o/product4.jpg?alt=media&token=a0bb3264-ee4b-42a9-a548-b1af0d05d4f9"
+                  },
+                  "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "text": "ก๋วยเตี๋ยวไก่ตุ๋นสมุนไพร",
+                        "wrap": true,
+                        "weight": "bold",
+                        "size": "xl"
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "69 บาท",
+                            "wrap": true,
+                            "weight": "bold",
+                            "size": "xl"
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  "footer": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "button",
+                        "style": "primary",
+                        "action": {
+                          "type": "uri",
+                          "label": "สั่งซื้อสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      },
+                      {
+                        "type": "button",
+                        "action": {
+                          "type": "uri",
+                          "label": "ดูรายละเอียดสินค้า",
+                          "uri": "https://linecorp.com"
+                        }
+                      }
+                    ]
+                  }
                 }
               ]
             }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "primary",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              }
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to wishlist",
-                "uri": "https://linecorp.com"
-              }
-            }
-          ]
-        }
-      },
-      {
-        "type": "bubble",
-        "hero": {
-          "type": "image",
-          "size": "full",
-          "aspectRatio": "20:13",
-          "aspectMode": "cover",
-          "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Metal Desk Lamp",
-              "wrap": true,
-              "weight": "bold",
-              "size": "xl"
-            },
-            {
-              "type": "box",
-              "layout": "baseline",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "$11",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "xl"
-                },
-                {
-                  "type": "text",
-                  "text": ".99",
-                  "wrap": true,
-                  "weight": "bold",
-                  "size": "sm"
-                }
-              ]
-            },
-            {
-              "type": "text",
-              "text": "Temporarily out of stock",
-              "wrap": true,
-              "size": "xxs",
-              "margin": "md",
-              "color": "#ff5551"
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "style": "primary",
-              "color": "#aaaaaa",
-              "action": {
-                "type": "uri",
-                "label": "Add to Cart",
-                "uri": "https://linecorp.com"
-              }
-            },
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Add to wish list",
-                "uri": "https://linecorp.com"
-              }
-            }
-          ]
-        }
-      },
-      {
-        "type": "bubble",
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "spacing": "sm",
-          "contents": [
-            {
-              "type": "button",
-              "gravity": "center",
-              "action": {
-                "type": "uri",
-                "label": "See more",
-                "uri": "https://linecorp.com"
-              }
-            }
-          ]
-        }
-      }
-    ]
-  }
-}]
+          }]
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
